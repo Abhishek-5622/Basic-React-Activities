0a7547e97250b0f05dc30cdfcb0467c76b20cc8a
Abhishek Mehta <abhishekmehta765@gmail.com>
2021-07-03 15:18:55 +0530

    FireBase Introduction

---
+++ demo_firebase/src/Component/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+firebase.initializeApp(
+{
+    apiKey: "AIzaSyDXUCOd3daZn9dJBArmO1B92b19u1I3IF4",
+    authDomain: "class-demo-abed7.firebaseapp.com",
+    projectId: "class-demo-abed7",
+    storageBucket: "class-demo-abed7.appspot.com",
+    messagingSenderId: "146212040824",
+    appId: "1:146212040824:web:2947b8a747316639223d8d"
+  
+  })
+
+  export default firebase;
